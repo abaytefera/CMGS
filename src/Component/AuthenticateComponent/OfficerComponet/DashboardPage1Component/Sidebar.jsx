2302709a17318c5edfd5cd112f236cca6ac31dce
abaytefera <man29@gmail.com>
2026-02-03 05:42:16 +0300

    last updtae

--- src/Component/AuthenticateComponent/OfficerComponet/DashboardPage1Component/Sidebar.jsx
+++ src/Component/AuthenticateComponent/OfficerComponet/DashboardPage1Component/Sidebar.jsx
@@ -113,7 +113,7 @@ const Sidebar = () => {
         <div className="flex flex-col h-full p-4 overflow-hidden">
           <div className="flex-shrink-0  self-center mb-8 px-2">
             <Link to={dashboardUrl} onClick={() => setIsOpen(false)}>
-              <img src="/logo2.jpg" alt="Logo" className="w-30 h-20" />
+              <img src="/logo2.jpg" alt="Logo" className="w-30 h-30" />
             </Link>
           </div>
 
